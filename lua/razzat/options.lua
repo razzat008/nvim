966d953e7e3d144e8997aca21d3a38a5895fe337
ceaser008 <radiusrazzat111@gmail.com>
2023-04-07 17:31:11 +0545

    options

--- lua/razzat/options.lua
+++ lua/razzat/options.lua
@@ -2,9 +2,38 @@ local o = vim.opt
 local g = vim.g
 keymap = vim.keymap.set
 
--- options relating to number
-o.rnu = true
-o.nu = true
 
 -- other options
 o.wrap = false
+o.guicursor = ""
+
+-- options relating to number
+o.nu = true
+o.rnu = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+o.smartindent = true
+
+o.wrap = false
+
+o.swapfile = true
+o.backup = false
+o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+
+o.hlsearch = false
+o.incsearch = true
+
+o.termguicolors = true
+
+o.scrolloff = 8
+o.signcolumn = "yes"
+o.isfname:append("@-@")
+
+o.updatetime = 50
+
+o.colorcolumn = "80"
